b80140cf1959939ffe8b7eec8ec0addec16d5923
Priti Biyani <pritib@in-pritibiyani.local>
2017-01-10 22:50:59 +0530

    Reading data from file and then movies list is displayed. A reading from file is considered as fetching exisiting api or repository layer

--- src/controllers/movie_service.py
+++ src/controllers/movie_service.py
@@ -1,16 +1,32 @@
+import os
+import json
+import re
+from settings import APP_DATA
 from models.movie import Movie
 
 
 class MovieService():
-    FILE_NAME = 'data/movie.json'
+    FILE_NAME = 'movie.json'
 
     def __init__(self):
         pass
 
+    def read_from_file(self):
+        with open(os.path.join(APP_DATA, self.FILE_NAME)) as file:
+            data = json.loads(file.read())
+        return data
+
     def fetch_movies(self):
-        toy_story = Movie("Toy Story", "A story of a boy and his toy",
-                          "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                          "https://www.youtube.com/watch?v=4KPTXpQehio")
         movie_list = []
-        movie_list.append(toy_story)
+        list = self.read_from_file()
+        for item in list['movies']:
+            trailer_id = self.get_youtube_id(item["youtube_trailer_url"])
+            movie = Movie(item["title"], item["storyline"], item["poster_image_url"], trailer_id)
+            movie_list.append(movie)
         return movie_list
+
+    def get_youtube_id(self, url):
+        youtube_id_match = re.search(r'(?<=v=)[^&#]+', url)
+        youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+', url)
+        trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match else None)
+        return trailer_youtube_id
--- src/models/movie.py
+++ src/models/movie.py
@@ -1,6 +1,6 @@
 class Movie():
-    def __init__(any, title, storyline, poster_image_url, youtube_trailer_url):
+    def __init__(any, title, storyline, poster_image_url, youtube_trailer_id):
         any.title = title
         any.storyline = storyline
         any.poster_image_url = poster_image_url
-        any.youtube_trailer_url = youtube_trailer_url
+        any.youtube_trailer_id = youtube_trailer_id
