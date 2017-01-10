a0128ea290c5ff5771ab1c3c179d3f22d2ccb9ef
Priti Biyani <pritib@in-pritibiyani.local>
2017-01-10 13:51:25 +0530

    Added app main class

---
+++ src/models/movie.py
@@ -0,0 +1,6 @@
+class Movie():
+    def __init__(any, title, storyline, poster_image_url, youtube_trailer_url):
+        any.title = title
+        any.storyline = storyline
+        any.poster_image_url = poster_image_url
+        any.youtube_trailer_url = youtube_trailer_url
